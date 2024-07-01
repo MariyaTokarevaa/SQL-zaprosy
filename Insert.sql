661f150d30df229f11ef77a4018938397b206189
MariyaTokarevaa <157952546+MariyaTokarevaa@users.noreply.github.com>
2024-07-01 14:08:08 +0400

    Add files via upload

---
+++ Insert.sql
@@ -0,0 +1,23 @@
+ INSERT INTO Genre(genre_name) 
+	VALUES ('Рок'), ('Поп'), ('Джаз'),('Классика');
+
+INSERT INTO Musician(musician_name) 
+	VALUES ('Виктор Цой'), ('Алла Пугачева'), ('Диана Кралл'), ('Людвиг ван Бетховен');
+
+INSERT INTO MusicianGenre(Genre_id, Musician_id)
+    VALUES (1, 1), (2, 2), (3, 3), (4, 4);
+    
+INSERT INTO Album(Album_name, Year_of_release_album)
+    VALUES ('Группа крови', 1988), ('Зеркало мира', 1983), ('The Look of Love', 2001), ('Симфония № 9', 1824);
+
+INSERT INTO AlbumMusician(Musician_id, Album_id)
+    VALUES (1, 1), (2, 2), (3, 3), (4, 4);
+   
+INSERT INTO Track(Track_name, Duration, Album_id)
+    VALUES ('Кукушка', 397, 1), ('Миллион алых роз', 355, 2), ('The Look of Love', 281, 3), ('Ода к радости', 1064, 4);
+
+INSERT INTO Collection(Collection_name, Year_of_release_album)
+    VALUES ('Лучшие хиты', 2020), ('Золотая коллекция', 2021), ('Джазовые стандарты', 2022), ('Классика для всех', 2023);
+   
+INSERT INTO TrackCollection(Track_id, Collection_id)
+    VALUES (1, 1), (2, 2), (3, 3), (4, 4);
